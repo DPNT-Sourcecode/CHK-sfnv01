098c116a12e5ab5a8b5a2780dee771d7086cac2f
sbx_user1051 <sbx_user1051@169.254.20.149>
2023-07-22 14:09:05 +0000

    Sat Jul 22 14:09:05 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,52 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
-special_offer_prices = {"A": [(5, 200),(3, 130)], "B": [(2, 45)]}
-special_free_items = {"E": ("B", 2), "F": ("F", 2)}
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40,
+    "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
+}
+special_offer_prices = {
+    "A": [(5, 200), (3, 130)],
+    "B": [(2, 45)],
+    "H": [(5, 45), (10, 80)],
+    "K": [(2, 150)],
+    "P": [(5, 200)],
+    "Q": [(3, 80)],
+    "V": [(2, 90), (3, 130)],
+}
+special_free_items = {
+    "E": ("B", 2),
+    "F": ("F", 2),
+    "N": ("M", 3),
+    "R": ("Q", 3),
+    "U": ("U", 3),
+}
+
+
 def checkout(skus):
     items = prices.keys()
     item_count = {key: 0 for key in items}
@@ -33,3 +74,4 @@ def checkout(skus):
         total_price += count * prices[item]
 
     return total_price
+
