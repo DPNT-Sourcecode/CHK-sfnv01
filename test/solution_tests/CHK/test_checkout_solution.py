373f24e6007919e65d0336787e1aea59ac2a0ee7
sbx_user1051 <sbx_user1051@169.254.20.149>
2023-07-22 12:36:14 +0000

    Sat Jul 22 12:36:14 UTC 2023

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -3,10 +3,13 @@ from lib.solutions.CHK import checkout_solution
 
 class TestSum():
     def test_checkout(self):
-        # assert checkout_solution.checkout('A') == 50
+        assert checkout_solution.checkout('A') == 50
         assert checkout_solution.checkout('D') == 15
-        # assert checkout_solution.checkout('ABCD') == 115
-        # assert checkout_solution.checkout('ABCD') == 115
-        # assert checkout_solution.checkout('ABCD') == 115
+        assert checkout_solution.checkout('AAA') == 130
+        assert checkout_solution.checkout('BBB') == 75
+        assert checkout_solution.checkout('AAABBCD') == 210
+        assert checkout_solution.checkout('AAABBBD') == 220
+        assert checkout_solution.checkout('E') == -1
+
 
 
